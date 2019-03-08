674e9cc6bb9081d762dd1be740836add01e5c40c
Nikolay Kelov <n_kelov@yahoo.com>
2019-03-08 19:57:28 +0100

    Problem 1, 60 out of 100 points

--- ConsoleApp1/ChristmasSpirit.cs
+++ ConsoleApp1/ChristmasSpirit.cs
@@ -6,7 +6,62 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-          
+            int quantity = int.Parse(Console.ReadLine());
+            int daysUntilChristmas = int.Parse(Console.ReadLine());
+
+            long ornamentSet = 2;
+            long treeSkirt = 5;
+            long treeGarlands = 3;
+            long treeLights = 15;
+            long christmasSpirit = 0;
+            long budget = 0;
+
+            for (int day = 1; day <= daysUntilChristmas; day++)
+            {
+                if (day % 2 == 0)
+                {
+                    budget += ornamentSet * quantity;
+                    christmasSpirit += 5;
+                }
+
+                if (day % 3 == 0)
+                {
+                    budget += (treeSkirt * quantity) + (treeGarlands * quantity);
+                    christmasSpirit += 13;
+                }
+
+                if (day % 5 == 0)
+                {
+                    budget += treeLights * quantity;
+                    christmasSpirit += 17;
+
+                    if (day % 3 == 0)
+                    {
+                        christmasSpirit += 30;
+                    }
+                }
+
+                if (day % 10 == 0)
+                {
+                    christmasSpirit -= 20;
+                    budget += treeSkirt + treeGarlands + treeLights;
+                    if (day == daysUntilChristmas)
+                    {
+                        christmasSpirit -= 30;
+                    }
+                }
+
+                if (day % 11 == 0)
+                {
+                    quantity += 2;
+                }
+
+
+            }
+
+            Console.WriteLine($"Total cost: {budget}");
+            Console.WriteLine($"Total spirit: {christmasSpirit}");
+
         }
     }
 }
