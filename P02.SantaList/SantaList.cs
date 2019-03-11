1c891df68bcd9762aa9889fd1a7cf5a6ae76ceaf
Nikolay Kelov <n_kelov@yahoo.com>
2019-03-11 08:36:17 +0100

    Problem 2, 100 out of 100 points.

--- P02.SantaList/SantaList.cs
+++ P02.SantaList/SantaList.cs
@@ -13,15 +13,49 @@ namespace P02.SantaList
 
             while (command != "Finished!")
             {
-                List<string> commandToList = command.Split().ToList;
-                string toDO = commandToList[0];
+                List<string> commandToList = command.Split().ToList();
+                string toDo = commandToList[0];
                 string name = commandToList[1];
 
+                if (toDo == "Bad")
+                {
+                    if (!badKids.Contains(name))
+                    {
+                        badKids.Insert(0, name);
+                    }
 
-
+                }
+                else if (toDo == "Good")
+                {
+                    if (badKids.Contains(name))
+                    {
+                        badKids.Remove(name);
+                    }
+                }
+                else if (toDo == "Rename")
+                {
+                    string newName = commandToList[2];
+                    if (badKids.Contains(name))
+                    {
+                        int index = badKids.IndexOf(name);
+                        badKids.RemoveAt(index);
+                        badKids.Insert(index, newName);
+                    }
+                }
+                else if (toDo == "Rearrange")
+                {
+                    if (badKids.Contains(name))
+                    {
+                        int index = badKids.IndexOf(name);
+                        badKids.RemoveAt(index);
+                        badKids.Add(name);
+                    }
+                }
 
                 command = Console.ReadLine();
             }
+
+            Console.WriteLine(string.Join(", ", badKids));
         }
     }
 }
